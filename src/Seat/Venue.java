8bfc87a320a22320a793328ed7eb3f89614a62e8
Qin Long <qinlonggoh@gmail.com>
2023-09-14 22:21:45 +0800

    venue

---
+++ src/Seat/Venue.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Seat;
+
+/**
+ *
+ * @author User
+ */
+public class Venue {
+    
+}

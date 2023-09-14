c9cccd77f69ea5bc76673754a8706577e4f4205b
Simon Yong <51854826+simony666@users.noreply.github.com>
2023-09-14 22:09:13 +0800

    Update Customer.java

--- src/Customer/Customer.java
+++ src/Customer/Customer.java
@@ -1,11 +1,14 @@
 package Customer;
 
 import util.Person;
+import util.Role;
 
 /**
  *
  * @author yongc
  */
 public class Customer extends Person{
-    
+    public Customer(String name, int age, Role role){
+        super(name,age,role);
+    }
 }

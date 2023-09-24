68c1632d7d0322ecf5f23bb7861b757110902457
Zzz-anonymous <leongzy-wp21@student.tarc.edu.my>
2023-09-24 12:54:25 +0800

    artist 3.0 modify

--- src/Artist/Artist.java
+++ src/Artist/Artist.java
@@ -1,12 +1,18 @@
 package Artist;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import util.Database;
+<<<<<<< Updated upstream
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+=======
+>>>>>>> Stashed changes
 
 /**
  *
@@ -16,7 +22,11 @@ public class Artist extends util.Person {
     
     // non-static field
     private int id;
+<<<<<<< Updated upstream
     private Database db;
+=======
+    private static Database db = new Database("C:/Users/User/OneDrive/ruyan/TAR UC/oop/oop assignment database/assignment database.db");
+>>>>>>> Stashed changes
 //    private String name;
 //    private int age;
 //    private Role role;
@@ -29,6 +39,7 @@ public class Artist extends util.Person {
     
     // no arg constructor
     public Artist(){
+        getFromDatabase();
     };
     
     // Constructor with 4 parameters
@@ -84,6 +95,7 @@ public class Artist extends util.Person {
 //    }
    
     public static void setArtistArrayList(ArrayList<Artist> artistList) {
+<<<<<<< Updated upstream
         artistArrayList = artistList;
         
         Connection conn = db.getConnection();
@@ -102,4 +114,58 @@ public class Artist extends util.Person {
         
     }
     
+=======
+    artistArrayList = artistList; // Assuming artistArrayList is a class variable
+
+//    Connection conn = null;
+//    ResultSet result = null;
+//
+//    try {
+//        // Establish a database connection
+//        conn = db.getConnection(); // Assuming db is an instance of your database utility class
+//
+//        // Create a SQL statement
+//        stmt = conn.createStatement();
+//
+//        // Execute a SQL query
+//        String sqlQuery = "SELECT * FROM your_table_name"; // Replace 'your_table_name' with the actual table name
+//        result = stmt.executeQuery(sqlQuery);
+//
+//        
+//    } catch (SQLException ex) {
+//        ex.printStackTrace(); // Handle the exception appropriately, e.g., logging or error handling
+//    } finally {
+//        // Close resources in reverse order of their creation
+//        try {
+//            if (result != null) {
+//                result.close();
+//            }
+//            if (stmt != null) {
+//                stmt.close();
+//            }
+//            if (conn != null) {
+//                conn.close();
+//            }
+//        } catch (SQLException ex) {
+//            ex.printStackTrace(); // Handle any exceptions that occur while closing resources
+//        }
+//    }
+}
+    private void getFromDatabase(){
+        // connect to database
+        // get data from databse (select statement)
+        // save data into arrayList
+//        while (result.next()) {
+//            // Retrieve data from the result set
+//            int id = result.getInt("Id"); // Assuming Id is an integer column
+//            String name = result.getString("Name"); // Assuming Name is a string column
+//
+//            // Create an Artist object or process the data as needed
+//            Artist artist = new Artist(id, name); // Replace with your Artist class constructor
+//
+//            // Add the artist to your ArrayList
+//            artistList.add(artist);
+//        }
+    }
+>>>>>>> Stashed changes
 }

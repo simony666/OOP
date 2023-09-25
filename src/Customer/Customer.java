5ea931ba5da279f9341031f40fd2395b3945f8ab
Simon Yong <51854826+simony666@users.noreply.github.com>
2023-09-26 07:46:03 +0800

    Create User/Customer/Staff

--- src/Customer/Customer.java
+++ src/Customer/Customer.java
@@ -1,14 +1,28 @@
 package Customer;
 
-import util.Person;
+import util.User;
 import util.Role;
 
 /**
  *
  * @author yongc
  */
-public class Customer extends Person{
-    public Customer(String name, int age, Role role){
-        super(name,age,role);
+public class Customer extends User{
+    
+    
+    
+    public Customer(String username, String email, String name, Role role) {
+        super(username,email,name,role);
     }
+    
+    public static Customer login(String username, String password){
+        User user = User.login(username,password);
+        if (user == null){
+            return null;
+        }else{
+            return new Customer(user.getUsername(),user.getEmail(),user.getName(),user.getRole());
+        }
+    }
+    
+    
 }
